c863079653776237e101985c50d8fc51fb8b2d6d
Dimitris Sideris <mangateersd@hotmail.com>
2017-11-01 20:52:25 +0200

    Add files via upload

--- project/trie.c
+++ project/trie.c
@@ -1,6 +1,6 @@
 #include "trie.h"
 
-
+// creates a node
 trie_node* create_trie_node(){
 
 	trie_node* created = malloc(sizeof(trie_node)*STARTNODESIZE);
@@ -9,15 +9,34 @@ trie_node* create_trie_node(){
 		printf("MEMERROR\n");
 		return NULL;
 	}
-
-	created->word[0] = '\0';
-	created->final = false;
-	created->children = NULL;
-	created->size = 0;
-
+	int i;
+	for(i=0;i<STARTNODESIZE;i++){
+		created[i].word[0] 	= '\0';
+		created[i].final 		= false;
+		created[i].children 	= NULL;
+		created[i].capacity 	= 0;
+		created[i].added 		= 0;		
+	}
 	return created;
 }
 
+trie_node*	enlarge_node(trie_node* mynode,int* mysize){
+
+	if(mynode==NULL){
+		printf("no node... creating node\n");
+
+		return create_trie_node();
+	}
+	trie_node* enlargednode = realloc(mynode,(*mysize)*sizeof(trie_node));
+	if(enlargednode == NULL){
+		printf("MEMERROR");
+		return NULL;
+	}
+	*mysize *=2;
+	return enlargednode;
+}
+
+// creates empty Trie
 trie* init_Trie(){
 
 	trie* created = malloc(sizeof(trie));
@@ -27,36 +46,112 @@ trie* init_Trie(){
 		return NULL;
 	}
 
-	created->root = create_trie_node();
+	created->root = NULL;
+	 /*create_trie_node();
 
 	if(created->root==NULL){
 		printf("MEMERROR\n");
 		free(created);
 		return NULL;
-	}
-	created->rootsize 	= STARTNODESIZE;
+	}*/
+	created->added 		= 0; //STARTNODESIZE;
+	created->capacity	= 0;
 	created->treesize 	= 0;
 	created->Ngrams 	= 0;
 
 	return created;
 }
 
+//
+int  insert_ngram(trie* ind,Ngram* adding_this){
+	
+	if(ind==NULL){
+		printf("NO TRIE\n");
+		return -1;
+	}
+	
+	if(adding_this==NULL){
+		printf("NO NGRAM\n");
+		return -2;
+	}
+
+	trie_node * currentnode = ind->root, nextnode = NULL;
+	int  i, level = 0;
+	int*  maxcap = &ind->capacity, cap_used = &ind->added;  	// needed?
+
+	for(i=0; i< adding_this->added; i++){
+		char* word = adding_this->wordBuffer[i];
+
+		if(binary_search(currentnode, word, cap_used)==0){ // FOUND
+				// is it last then mark final!
+		}
+		else{ // not inside add
+			if(maxcap* == cap_used*){	// if full double
+				ind->root = enlarge_node(ind->root, &(ind->capacity));
+			}
+
+			// add it and sort it 
+		}
+
+	}
+
+	return 0;
+}
+
+int binary_search(trie_node * currentnode, char* word,int mysize){
+	int small = 0, int big = mysize, middle = (small+big)/2;
+	
+	while (small <= big) {
+	      
+	      	if (strcmp(currentnode[middle].word, word) == 0){ 			//FOUND
+	      		printf("%s found at location %d.\n", word, middle+1);
+	      		return 0;	 
+	      	}  
+	      	else if (strcmp(currentnode[middle].word, word) < 0){
+	         	big = middle + 1;
+	        	
+	      	}
+	      	else
+	         	small = middle - 1;
+	 
+	      middle = (small + big)/2;
+	}
+
+	return -1; 	
+}
+
+
+int  delete_ngram(trie* ind,Ngram* deleting_this){
+
+	return 0;
+}
+
+
+char* search(trie* ind, Ngram* searching_this){
+
+	return 0;
+}
+
+
+
+
+// helps in destruction of trie different than delete_ngram
 void delete_node(trie_node* node){
 	
 	if(node!=NULL){
 		int i =0;
-		for(; i < node->size; i++){
+		for(; i < node->capacity; i++){
 
 			delete_node(&(node->children[i]));
 		}
 		free(node);
 	}
 }
-
+// destroys whole trie
 void delete_index(trie** ind){
 	if(*ind!= NULL){
 		int  i;
-		for(i=0;i<(*ind)->rootsize; i++){
+		for(i=0;i<(*ind)->capacity; i++){
 			delete_node(&((*ind)->root[i]));
 		}
 		free(*ind);
--- project/trie.h
+++ project/trie.h
@@ -9,28 +9,37 @@
 typedef struct trie_node trie_node;
 
 struct trie_node{
-	char word [WORDLIMIT];
-	bool final;
-	trie_node* children;
-	int size;
+
+	char 		word[WORDLIMIT];
+	bool 		final;
+	trie_node* 	children;
+	int 		capacity;
+	int 		added;
+
 };
 
 
 typedef struct trie{
-	trie_node* root;
-	int rootsize;
-	int treesize;
-	int Ngrams;
+	
+	trie_node* 	root;
+	int 		capacity;
+	int 		added;
+	int 		treesize;
+	int 		Ngrams;
+
 }trie;
 
 trie* init_Trie();
 trie_node* create_trie_node();
+//OK SUCCESS?!
 
 int  insert_ngram(trie* ind,Ngram* adding_this);
 
 int  delete_ngram(trie* ind,Ngram* deleting_this);
 
 char* search(trie* ind, Ngram* searching_this);
+
+int binary_search(trie_node * currentnode, char* word,int mysize);
 
 void delete_index(trie** ind);
 void delete_node(trie_node* node);
