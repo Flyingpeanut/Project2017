cd0aacf6488a8b2c74a688f1d490440867070df5
Dimitris Sideris <mangateersd@hotmail.com>
2017-11-03 02:10:32 +0200

    Add files via upload

--- project/header.h
+++ project/header.h
@@ -9,16 +9,18 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-#define WORDLIMIT 50
-#define LINELIMIT 2000
-#define DELETE 		"D"
-#define ADD			"A"
-#define QUERY		"Q"
-#define FINAL		"F"
-#define OK_SUCCESS  0
-#define	ARG_ERROR 	1
-#define MEMERR  	2
-#define BADUSE		3
-#define FILEOPENINGERROR -1
+#define 	WORDLIMIT 			50
+#define 	LINELIMIT 			2000
+#define 	DELETE 				"D"
+#define 	ADD					"A"
+#define 	QUERY				"Q"
+#define 	FINAL				"F"
+#define 	OK_SUCCESS  		0
+#define		ARG_ERROR 			1
+#define	 	MEMERR  			2
+#define 	BADUSE				3
+#define 	NOTFOUND			4
+#define 	FILEOPENINGERROR 	-1
+
 
 #endif
